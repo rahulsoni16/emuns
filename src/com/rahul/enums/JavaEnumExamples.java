2dfe269658910f5880a8b1c8dfb672289f898512
Rahul Soni <rahul.soni16@gmail.com>
2017-12-15 17:23:59 +0530

    Test1

--- src/com/rahul/enums/JavaEnumExamples.java
+++ src/com/rahul/enums/JavaEnumExamples.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class JavaEnumExamples {
 
 	public static void main(String[] args) throws IOException {
-		//Test		
+		//Test1		
 		usingEnumMethods();
 		
 		usingEnumValueOf();
