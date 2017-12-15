11e956e00c92a52afa66586ada7972d75b44a8e1
Rahul Soni <rahul.soni16@gmail.com>
2017-12-15 12:36:04 +0530

    One more commit

--- src/com/rahul/enums/JavaEnumExamples.java
+++ src/com/rahul/enums/JavaEnumExamples.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class JavaEnumExamples {
 
 	public static void main(String[] args) throws IOException {
-				
+		//Test		
 		usingEnumMethods();
 		
 		usingEnumValueOf();
